b53f78426b9f86e215a96f57d0c03aa14cc8c029
Alim Lyukmanov <50545653+desueBuyer@users.noreply.github.com>
2020-02-27 15:17:45 +0300

    Add files via upload

---
+++ task1/task1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null)
+            {
+                string path = args[0];
+                List<int> list = LinesFromFile(path);
+                if (list != null)
+                {
+                    int[] mass = list.ToArray();
+                    Array.Sort(mass);
+                    Counter counter = new Counter { Mass = mass };
+                    Console.WriteLine(string.Format("{0:N2}", counter.Percentile_90()));
+                    Console.WriteLine(string.Format("{0:N2}", counter.Median()));
+                    Console.WriteLine(string.Format("{0:N2}", counter.Max()));
+                    Console.WriteLine(string.Format("{0:N2}", counter.Min()));
+                    Console.WriteLine(string.Format("{0:N2}", counter.Mean()));
+                }
+                Console.ReadLine();
+            }
+        }
+
+        static  List<int> LinesFromFile(string path)
+        {
+            List<int> list = new List<int>();
+             string line = null;
+             using (StreamReader sr = new StreamReader(path))
+             {
+                while (true)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                    if (line == null) break;
+                    list.Add(Int32.Parse(line));
+                }
+             }
+            return list;
+        }
+        class Counter
+        {
+            public int[] Mass { get; set; }
+            
+            public double Percentile_90()
+            {
+                double n = (double)90 / 100 * (Mass.Length - 1) + 1;
+                double percentile = Mass[Convert.ToInt32(n)-1] + (Convert.ToDouble(Mass[Convert.ToInt32(n)]- Mass[Convert.ToInt32(n) - 1])) * (n - Convert.ToInt32(n));
+                return  percentile;
+            }
+            public double Median()
+            {
+                if (Mass.Length%2==0)
+                    return (Convert.ToDouble(Mass[(Mass.Length-1)/ 2]) + Convert.ToDouble(Mass[(Mass.Length-1) /2 ])) / 2;
+                return Convert.ToDouble(Mass[Mass.Length / 2]);
+            }
+
+            public double Min()
+            {
+                return Convert.ToDouble(Mass[0]);
+            }
+
+            public double Max()
+            {
+                return Convert.ToDouble(Mass[Mass.Length-1]);
+            }
+
+            public double Mean()
+            {
+                double sum = 0;
+
+                foreach (var e in Mass)
+                    sum += e;
+                return sum / Mass.Length;
+            }
+             
+        }
+    }
+}
---
+++ task3/task3.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace task3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            float[,] queues = Queues(args);
+            Console.WriteLine(MaxQueue(queues));
+            Console.ReadLine();
+        }
+
+        static List<float> LinesFromFile(string path)
+        {
+            List<float> list = new List<float>();
+            List<string> lines = File.ReadAllLines(path).ToList();
+            foreach (var elem in lines)
+                list.Add(float.Parse(elem));
+            return list;
+        }
+
+        static float[,] Queues(string[] args)
+        {
+            string[] files = { "Cash1.txt", "Cash2.txt", "Cash3.txt", "Cash4.txt", "Cash5.txt" };
+            float[,] queues = new float[5, 16];
+            for (int i = 0; i < 5; i++)
+            {
+                string finalPath = null;
+                finalPath += args[0] + "\\" + files[i];
+                List<float> queue = LinesFromFile(finalPath);
+                for (int j = 0; j < 16; j++)
+                {
+                    queues[i, j] = queue[j];
+                }
+            }
+            return queues;
+        }
+
+        static int MaxQueue(float[,] queues)
+        {
+            float max = 0;
+            int maxAvgId = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                float sum = 0;
+                for (int j = 0; j < 5; j++)
+                    sum += queues[j, i];
+                if (sum > max)
+                {
+                    max = sum;
+                    maxAvgId = i + 1;
+                }
+            }
+            return maxAvgId;
+        }
+    }
+}
+    
+
